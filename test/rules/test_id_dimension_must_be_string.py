b3b91ccecf442f49ad3145754fb03d85e6fd34cb
nikita-chikishev <nikita.chikishev@printful.com>
2025-12-17 23:48:15 +0100

    added test scenarios to test_id_dimension_must_be_string.py

--- test/rules/test_id_dimension_must_be_string.py
+++ test/rules/test_id_dimension_must_be_string.py
@@ -1,10 +1,46 @@
-import unittest
+from linter.rule import Severity
+from linter.rules.id_dimension_must_be_string import IdDimensionMustBeString
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+def test_run_method_successfully_validates_merchant_id_string() -> None:
+    rule = IdDimensionMustBeString(Severity.ERROR.value)
+
+    field = {
+        'name': 'merchant_id',
+        'type': 'string'
+    }
+    rule_result = rule.run(field)
+    assert rule_result == True
+
+def test_run_method_fails_merchant_id_number() -> None:
+    rule = IdDimensionMustBeString(Severity.ERROR.value)
+
+    field = {
+        'name': 'merchant_id',
+        'type': 'number'
+    }
+    rule_result = rule.run(field)
+    assert rule_result == False
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_run_method_fails_sql_reference_number() -> None:
+    rule = IdDimensionMustBeString(Severity.ERROR.value)
+
+    field = {
+        'name': 'merchant_pk',
+        'type': 'number',
+        'sql': '${TABLE}.merchant_id'
+    }
+    rule_result = rule.run(field)
+    assert rule_result == False
+
+def test_run_method_ignores_unrelated_field() -> None:
+    rule = IdDimensionMustBeString(Severity.ERROR.value)
+
+    field = {
+        'name': 'order_amount',
+        'type': 'number',
+        'sql': '${TABLE}.amount'
+    }
+    rule_result = rule.run(field)
+    assert rule_result == True
