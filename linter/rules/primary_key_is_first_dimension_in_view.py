224547a0d627a6616bab0501b500ed36502e771b
nikita-chikishev <nikita.chikishev@printful.com>
2025-12-18 14:15:19 +0100

    added skipping primary key order check for extends

--- linter/rules/primary_key_is_first_dimension_in_view.py
+++ linter/rules/primary_key_is_first_dimension_in_view.py
@@ -7,6 +7,9 @@ class PrimaryKeyIsFirstDimensionInView(Rule):
         return ('view',)
 
     def run(self, view: Any) -> bool:
+        # If the view extends another view, the Primary Key order check can be skipped.
+        if 'extends' in view:
+            return True
         dimensions = view.get('dimensions', [])
         if len(dimensions) > 0 and dimensions[0].get('primary_key') != 'yes':
             return False
